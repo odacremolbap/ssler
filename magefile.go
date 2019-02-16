2e137686412b2223e3041b98f4f8f3eb355c1e4d
odacremolbap <pablo@stackpointcloud.com>
2019-02-16 12:09:18 +0100

    initial build

---
+++ magefile.go
@@ -0,0 +1,74 @@
+// +build mage
+
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+const cmdPackage = "cmd"
+
+// List of binaries to build.
+// They should be packaged under $REPO/cmd/<binary name>
+var binaries = []string{"ssler"}
+
+var Default = Build
+
+// Build at the repository root folder
+func Build() error {
+	mg.Deps(Deps)
+	log.Println("Building...")
+	return build(".")
+}
+
+// Install at gopath bin directory
+func Install() error {
+	mg.Deps(Build)
+	log.Println("Installing...")
+	goPath, err := sh.Output("go", "env", "GOPATH")
+	if err != nil {
+		return err
+	}
+	if len(goPath) == 0 {
+		return errors.New("GOPATH is not set")
+	}
+
+	paths := strings.Split(goPath, string([]rune{os.PathListSeparator}))
+	binPath := filepath.Join(paths[0], "bin")
+	return build(binPath)
+}
+
+// Deps ensure dependencies
+func Deps() error {
+	log.Println("Ensuring dependencies...")
+	return sh.RunV("dep", "ensure")
+}
+
+// Clean up
+func Clean() error {
+	log.Println("Cleaning...")
+	for _, b := range binaries {
+		if err := os.RemoveAll(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func build(outPath string) error {
+	for _, b := range binaries {
+		file := path.Join(outPath, b)
+		if err := sh.RunV("go", "build", "-o", file, "./"+cmdPackage+"/"+b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
