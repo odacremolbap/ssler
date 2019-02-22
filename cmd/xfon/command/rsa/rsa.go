4ef2dbb7a1d7cd2c93b6c60fc9911d7e5b852bd6
odacremolbap <pablo@stackpointcloud.com>
2019-02-22 12:13:00 +0100

    write key to fs

--- cmd/xfon/command/rsa/rsa.go
+++ cmd/xfon/command/rsa/rsa.go
@@ -10,6 +10,7 @@ import (
 
 var (
 	bits int
+	out  string
 
 	// RootCmd manages private keys
 	RootCmd = &cobra.Command{
@@ -22,13 +23,7 @@ var (
 	NewCmd = &cobra.Command{
 		Use:   "new",
 		Short: "creates new RSA key",
-		Run: func(cmd *cobra.Command, args []string) {
-			_, err := cert.GenRSAKey(bits)
-			if err != nil {
-				log.Printf("error generating RSA key: %v", err.Error())
-				os.Exit(-1)
-			}
-		},
+		Run:   NewFunc,
 	}
 )
 
@@ -38,5 +33,37 @@ func runHelp(cmd *cobra.Command, args []string) {
 
 func init() {
 	NewCmd.PersistentFlags().IntVar(&bits, "bits", 4096, "key size")
+	NewCmd.PersistentFlags().StringVar(&out, "out", "", "RSA key output file")
 	RootCmd.AddCommand(NewCmd)
+}
+
+// NewFunc runs the new RSA command
+func NewFunc(cmd *cobra.Command, args []string) {
+	k, err := cert.GenerateRSAKey(bits)
+	if err != nil {
+		log.Printf("error generating RSA key: %v", err.Error())
+		os.Exit(-1)
+	}
+
+	p, err := cert.RSAtoPEM(k)
+	if err != nil {
+		log.Printf("error serializing RSA key into PEM: %v", err.Error())
+		os.Exit(-1)
+	}
+
+	err = writeCertFile(out, p)
+	if err != nil {
+		log.Printf("error writing RSA key to file: %v", err.Error())
+		os.Exit(-1)
+	}
+}
+
+func writeCertFile(path, contents string) error {
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(contents)
+	return err
 }
