dd9464eff39ea8103025e9f592c760313320b8a0
odacremolbap <pablo@stackpointcloud.com>
2019-02-25 12:10:00 +0100

    add rsa encoding tests

--- pkg/rsa/rsa_test.go
+++ pkg/rsa/rsa_test.go
@@ -11,10 +11,10 @@ func TestGenerateKey(t *testing.T) {
 		testName string
 		keySize  int
 	}{
-		{testName: "t1",
+		{testName: "t4096",
 			keySize: 4096,
 		},
-		{testName: "t2",
+		{testName: "t1024",
 			keySize: 1024,
 		},
 	}
@@ -23,5 +23,27 @@ func TestGenerateKey(t *testing.T) {
 		err := key.Validate()
 		assert.Nil(t, err, "test: %s", td.testName)
 		assert.Equalf(t, td.keySize, key.N.BitLen(), "test: %s", td.testName)
+	}
+}
+
+func TestPEMEncode(t *testing.T) {
+	var testData = []struct {
+		testName string
+		keySize  int
+	}{
+		{testName: "t4096",
+			keySize: 4096,
+		},
+		{testName: "t1024",
+			keySize: 1024,
+		},
+	}
+	for _, td := range testData {
+		key, _ := GenerateKey(td.keySize)
+		pem, err := WritePEM(key)
+		assert.Nil(t, err, "test writePEM: %s", td.testName)
+		retKey, err := ReadPEM([]byte(pem))
+		assert.Nil(t, err, "test readPEM: %s", td.testName)
+		assert.Equalf(t, key, retKey, "test: %s", td.testName)
 	}
 }
